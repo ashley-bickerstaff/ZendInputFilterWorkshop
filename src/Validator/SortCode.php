799c44acdf4e31e3a64ea01de145b36e30f3f387
Ashley Bickerstaff <ashley.bickerstaff@workplacesystems.com>
2017-01-27 12:27:03 +0000

    Stubbed sort code validator

---
+++ src/Validator/SortCode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ZendInputFilterWorkshop\Validator;
+
+use Zend\Validator\AbstractValidator;
+
+class SortCode extends AbstractValidator
+{
+    public function isValid($value)
+    {
+        // TODO: Implement isValid() method.
+    }
+}
---
+++ tests/Validator/SortCodeTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ZendInputFilterWorkshop\Validator;
+
+use PHPUnit\Framework\TestCase;
+
+class SortCodeTest extends TestCase
+{
+
+}
